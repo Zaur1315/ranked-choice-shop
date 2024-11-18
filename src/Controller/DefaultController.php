8c0d674e69953a67eab1a31d920452114402c73e
Zaur Huseynzade <z.guseynzade@pay2net.co>
2024-11-18 17:53:42 +0400

    done

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Config\Framework\RequestConfig;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $entityManager = $doctrine->getManager();
+        $productList = $entityManager->getRepository(Product::class)->findAll();
+
+        dd($productList);
         return $this->render('main/default/index.html.twig', []);
+    }
+
+    #[Route('/edit-product/{id}', name: 'product_edit', requirements: ['id'=>'\d+'], methods: 'GET|POST')]
+    #[Route('/add-product/', name: 'product_add', methods: 'GET|POST')]
+    public function editProduct(ManagerRegistry $doctrine, Request $request, int $id = null): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        if ($id) {
+            $product = $entityManager->getRepository(Product::class)->find($id);
+        }else{
+            $product = new Product();
+        }
+
+        $form = $this->createFormBuilder($product)
+            ->add('title', TextType::class)
+            ->getForm();
+        dd($product, $form);
+        return $this->render('main/default/edit_product.html.twig', []);
     }
 }
---
+++ src/Entity/User.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    public function __construct()
+    {
+        $this->is_deleted = false;
+    }
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?bool $is_deleted = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function isDeleted(): ?bool
+    {
+        return $this->is_deleted;
+    }
+
+    public function setDeleted(bool $is_deleted): static
+    {
+        $this->is_deleted = $is_deleted;
+
+        return $this;
+    }
+}
