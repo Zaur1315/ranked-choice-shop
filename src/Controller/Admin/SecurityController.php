e2c54c42b3ffaf5b4e67a2d9cf3422ec7f268907
Zaur Huseynzade <z.guseynzade@pay2net.co>
2024-12-05 16:31:42 +0400

    admin panel

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -13,7 +13,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('main_profile_index');
+            return $this->redirectToRoute('main_homepage');
         }
 
         // get the login error if there is one
--- src/Security/Admin/LoginFormAuthenticator.php
+++ src/Security/Admin/LoginFormAuthenticator.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -25,27 +26,29 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'admin_security_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private UrlGeneratorInterface $urlGenerator, private ManagerRegistry $doctrine)
     {
     }
 
     public function authenticate(Request $request): Passport
     {
         $email = $request->getPayload()->getString('email');
+        $password = $request->getPayload()->getString('password');
+        $csrfToken = $request->getPayload()->getString('_csrf_token');
 
         $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
 
         return new Passport(
-            new UserBadge($email, function ($userIdentifier, ManagerRegistry $doctrine, $credentials) {
-                $user = $this->$doctrine->getRepository(User::class)->findOneBy(['email' => $credentials['email']]);
-                if (!$user || !$user->hasRole('ROLE_ADMIN')) { // Проверка роли
-                    throw new UserNotFoundException('Email could not be found');
+            new UserBadge($email, function (string $userIdentifier): UserInterface {
+                $user = $this->doctrine->getRepository(User::class)->findOneBy(['email' => $userIdentifier]);
+                if (!$user || !in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+                    throw new UserNotFoundException();
                 }
                 return $user;
             }),
-            new PasswordCredentials($request->getPayload()->getString('password')),
+            new PasswordCredentials($password),
             [
-                new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
+                new CsrfTokenBadge('authenticate', $csrfToken),
                 new RememberMeBadge(),
             ]
         );
@@ -57,7 +60,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('main_profile_index'));
+        return new RedirectResponse($this->urlGenerator->generate('admin_dashboard_show'));
     }
 
     protected function getLoginUrl(Request $request): string
